70cc7f3d7b0f8008faf1618926ce628c1a9eaabd
vagrant <nshankar@ufl.edu>
2013-10-07 19:48:54 +0000

    Bug fix - namespace

--- src/mnshankar/RoleBasedAuthority/RoleBasedAuthorityServiceProvider.php
+++ src/mnshankar/RoleBasedAuthority/RoleBasedAuthorityServiceProvider.php
@@ -21,13 +21,13 @@ class RoleBasedAuthorityServiceProvider extends ServiceProvider
      */
     public function register()
     {        
-        $this->package('mnshankar/RoleBasedAuthority');
+        $this->package('mnshankar/role-based-authority');
         
         $this->app['authority'] = $this->app->share(function ($app)
         {
             $user = $app['auth']->user();
-            $authority = new Authority($user);
-            $fn = $app['config']->get('authority-l4::initialize', null);
+            $authority = new \Authority\Authority($user);
+            $fn = $app['config']->get('role-based-authority::initialize', null);
             
             if ($fn) {
                 $fn($authority);
